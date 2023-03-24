fbdc111d41b41d25ce4ee1fba20593354ffe17ab
JoFrhwld <JoFrhwld@gmail.com>
2023-03-24 18:01:22 -0400

    prob/cont function. closes #1

--- R/sim_funs.R
+++ R/sim_funs.R
@@ -93,5 +93,46 @@ sim_mixture <- function(
   return(out)
 }
 
+#' simulate prob/continuous change
+#'
+#' @param max_year The total number of "years" the change is spread across
+#' @param pop_size The total number of simulated data points
+#' @param phi The "precision" of the beta distribution
+#' @param n Number of simulated values
+#' @param prob Probability of distribution 1
+#' @param mu1 Mean of distrubtion 1
+#' @param sigma1 sd of distribution 1
+#' @param mu2 mean of distribution2
+#' @param sigma2 sd of distribution 2
+#'
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr mutate
+#' @importFrom tidyr unnest
+#' @export
+sim_prob_cont <- function(
+    max_year = 100,
+    pop_size = 300,
+    phi = 10,
+    n = 50,
+    prob = 0.2,
+    mu1=1,
+    sigma1=0.25,
+    mu2=0,
+    sigma2=0.25
+){
+  prob_df <- sim_prob_change(max_year = max_year, pop_size = pop_size, phi = phi)
+  prob_df |>
+    rowwise() |>
+    mutate(
+      continuous = list(sim_mixture(prob = prob_obs))
+    ) |>
+    unnest(continuous)->
+    full_sim
+  return(full_sim)
+}
+
+
+
+
 #' inverse logit
 inv_logit <- \(x) exp(x)/(1+exp(x))
